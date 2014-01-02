ce2c502f61889d2824a0b0adfb4a26013891b958
Brad Dunbar <dunbarb2@gmail.com>
2014-01-02 11:13:10 -0500

    First.

---
+++ polyfill.js
@@ -0,0 +1,205 @@
+(function() {
+
+var Promise = typeof exports !== 'undefined'
+  ? require('es6-promise').Promise
+  : window.Promise
+;
+
+var _ = typeof exports !== 'undefined'
+  ? require('underscore')
+  : window._
+;
+
+// http://dev.w3.org/html5/webdatabase
+var DB = module.exports = function(name, version, schema) {
+  this.name = name;
+  this.version = version;
+  this.schema = schema;
+};
+
+DB.drop = function(name) {
+  return new Promise(function(resolve, reject) {
+    var db = openDatabase(name, '', name, 5 * 1024 * 1024);
+    db.transaction(function(tx) {
+      var sql = "select tbl_name from sqlite_master where type = 'table'";
+      tx.executeSql(sql, [], function(tx, res) {
+        for (var i = 0; i < res.rows.length; i++) {
+          var name = res.rows.item(i).tbl_name;
+          tx.executeSql('drop table ' + name, [],
+            function() {},
+            function() { return false; }
+          );
+        }
+      });
+    },
+    function(e){ reject(e); },
+    function(){ resolve(); });
+  });
+};
+
+_.extend(DB.prototype, {
+
+  open: function() {
+    var self = this;
+    if (this.openPromise) return this.openPromise;
+    return this.openPromise = new Promise(function(resolve, reject) {
+      var db = openDatabase(self.name, '', self.name, 5 * 1024 * 1024);
+      db.transaction(function(tx) {
+        for (var name in self.schema) {
+          var options = self.schema[name];
+          var keyPath = options.keyPath;
+          var indices = options.indices;
+
+          tx.executeSql(
+            'create table if not exists ' + name + ' (id primary key asc, data)'
+          );
+
+          for (var index in indices) {
+            var indexOptions = indices[index];
+
+            // If the column exists, return false to avoid failing the
+            // transaction.
+            var sql = 'alter table ' + name + ' add column ' + index;
+            tx.executeSql(sql, [], function(){}, function(){ return false });
+
+            tx.executeSql(
+              'create index if not exists ' + index +
+              ' on ' + name + ' (' + index + ')'
+            );
+          }
+        }
+      },
+      function(e) { reject(e); },
+      function(){ resolve(db); });
+    });
+  },
+
+  close: function() {
+    return Promise.resolve();
+  },
+
+  clear: function(names) {
+    if (names == null) names = _.keys(this.schema);
+    if (!_.isArray(names)) names = [names];
+    names = _.filter(names, function(name){
+      return !!this.schema[name];
+    }, this);
+
+    return this.open().then(function(db) {
+      return new Promise(function(resolve, reject) {
+        db.transaction(function(tx) {
+          _.each(names, function(name) {
+            tx.executeSql('delete from ' + name, []);
+          });
+        },
+        function(e){ reject(e); },
+        function(){ resolve(); });
+      });
+    });
+  },
+
+  put: function(name, data) {
+    if (!_.isArray(data)) data = [data];
+    var store = this.schema[name] || {};
+    var keyPath = store.keyPath || 'id';
+    var indices = store.indices || {};
+
+    return this.open().then(function(db) {
+      return new Promise(function(resolve, reject) {
+        db.transaction(function(tx) {
+          _.each(data, function(item) {
+            var sql = 'insert or replace into ' + name + ' (id, data';
+            for (var index in indices) sql += ', ' + index;
+            sql += ') values (?, ?';
+            for (var index in indices) sql += ', ?';
+            sql += ')';
+
+            var args = [item[keyPath], JSON.stringify(item)];
+            for (var index in indices) args.push(item[index]);
+
+            tx.executeSql(sql, args);
+          });
+        },
+        function(e) { reject(e); },
+        function() { resolve(); });
+      });
+    });
+
+  },
+
+  get: function(name, key) {
+    var result;
+    return this.open().then(function(db) {
+      return new Promise(function(resolve, reject) {
+        db.transaction(function(tx) {
+          var sql = 'select data from ' + name + ' where id = ? limit 1';
+          tx.executeSql(sql, [key], function(tx, res) {
+            if (res.rows.length === 1) {
+              try { result = JSON.parse(res.rows.item(0).data); } catch(e){}
+            }
+          });
+        },
+        function(e){ reject(e); },
+        function(){ resolve(result); });
+      });
+    });
+  },
+
+  query: function(name, options) {
+    var page = options && options.page;
+    var order = options && options.order;
+    var per_page = options && options.per_page;
+    var sort_mode = options && options.sort_mode;
+
+    if (page != null && per_page == null) per_page = 10;
+
+    var store = this.schema[name];
+    var indices = _.keys(store && store.indices || {});
+    var res = {
+      results: [],
+      total_entries: 0
+    };
+
+    if (!this.schema[name]) return Promise.resolve(res);
+
+    return this.open().then(function(db) {
+      return new Promise(function(resolve, reject) {
+        db.transaction(function(tx) {
+          var sql = 'select count(id) as count from ' + name;
+
+          tx.executeSql(sql, [], function(tx, results) {
+            res.total_entries = results.rows.item(0).count;
+          });
+
+          sql = 'select data from ' + name + ' order by ';
+
+          // order
+          sql += _.contains(indices, order)
+            ? order + ' ' + (sort_mode || 'asc')
+            : 'id'
+          ;
+
+          // paging
+          if (page != null && per_page != null) {
+            sql += ' limit ' + per_page;
+            if (page > 1) sql += ', ' + (per_page * (page - 1));
+          }
+
+          tx.executeSql(sql, [], function(tx, results) {
+            for (var i = 0; i < results.rows.length; i++) {
+              try {
+                res.results.push(JSON.parse(results.rows.item(i).data));
+              } catch(e){}
+            }
+          });
+        },
+        function(e){ reject(e); },
+        function(){ resolve(res); });
+      });
+    });
+
+  }
+
+});
+
+}).call(this);
---
+++ test/index.js
@@ -0,0 +1,299 @@
+var tape = require('tape');
+var DB = require('../') || require('../polyfill');
+
+var test = function(name, options, callback) {
+  if (!callback) {
+    callback = options;
+    options = null;
+  }
+  tape(name, options, function(t) {
+    DB.drop('test').then(function() {
+      return callback(t);
+    })['catch'](function(e) {
+      t.fail(e.message + ' ' + e.stack);
+      t.end();
+    });
+  });
+};
+
+test('Put and get.', function(t) {
+  var db = new DB('test', 1, {test: {}});
+  return db.put('test', {id: 1, x: 1}).then(function() {
+    return db.get('test', 1).then(function(obj) {
+      t.same(obj, {id: 1, x: 1});
+      db.close().then(function(){ t.end(); });
+    });
+  });
+});
+
+test('Put and query', function(t) {
+  var db = new DB('test', 1, {test: {}});
+  return db.put('test', {id: 1, x: 1}).then(function() {
+    return db.query('test').then(function(res) {
+      t.same(res.results, [{id: 1, x: 1}]);
+      db.close().then(function(){ t.end(); });
+    });
+  });
+});
+
+test('Put and query', function(t) {
+  var db = new DB('test', 1, {test: {}});
+  return db.put('test', [{id: 1, x: 1}, {id: 2, x: 2}]).then(function() {
+    return db.query('test').then(function(res) {
+      t.same(res.results, [{id: 1, x: 1}, {id: 2, x: 2}]);
+      db.close().then(function(){ t.end(); });
+    });
+  });
+});
+
+test('Query order.', function(t) {
+  var db = new DB('test', 1, {test: {indices: {x: {}}}});
+  var models = [
+    {id: 1, x: 1},
+    {id: 3, x: 3},
+    {id: 2, x: 2}
+  ];
+  return db.put('test', models).then(function() {
+    return db.query('test', {order: 'x'})
+    .then(function(res) {
+      t.same(res.results, [
+        {id: 1, x: 1},
+        {id: 2, x: 2},
+        {id: 3, x: 3}
+      ]);
+      db.close().then(function(){ t.end(); });
+    });
+  });
+});
+
+test('Query order asc.', function(t) {
+  var db = new DB('test', 1, {test: {indices: {x: {}}}});
+  var models = [
+    {id: 1, x: 1},
+    {id: 3, x: 3},
+    {id: 2, x: 2}
+  ];
+  return db.put('test', models).then(function() {
+    return db.query('test', {order: 'x', sort_mode: 'asc'})
+    .then(function(res) {
+      t.same(res.results, [
+        {id: 1, x: 1},
+        {id: 2, x: 2},
+        {id: 3, x: 3}
+      ]);
+      db.close().then(function(){ t.end(); });
+    });
+  });
+});
+
+test('Query order asc.', function(t) {
+  var db = new DB('test', 1, {test: {indices: {x: {}}}});
+  var models = [
+    {id: 1, x: 1},
+    {id: 3, x: 3},
+    {id: 2, x: 2}
+  ];
+  return db.put('test', models).then(function() {
+    return db.query('test', {order: 'x', sort_mode: 'desc'})
+    .then(function(res) {
+      t.same(res.results, [
+        {id: 3, x: 3},
+        {id: 2, x: 2},
+        {id: 1, x: 1}
+      ]);
+      db.close().then(function(){ t.end(); });
+    });
+  });
+});
+
+test('Paging.', function(t) {
+  var db = new DB('test', 1, {test: {indices: {x: {}}}});
+  var models = [
+    {id: 1},
+    {id: 2},
+    {id: 3},
+    {id: 4}
+  ];
+  return db.put('test', models).then(function(){
+    return db.query('test', {page: 1, per_page: 3}).then(function(res) {
+      t.same(res.results, models.slice(0, 3));
+      db.close().then(function(){ t.end(); });
+    });
+  });
+});
+
+test('Paging.', function(t) {
+  var db = new DB('test', 1, {test: {indices: {x: {}}}});
+  var models = [
+    {id: 1},
+    {id: 2},
+    {id: 3},
+    {id: 4}
+  ];
+  return db.put('test', models).then(function() {
+    return db.query('test', {page: 2, per_page: 3}).then(function(res) {
+      t.same(res.results, models.slice(3));
+      db.close().then(function(){ t.end(); });
+    });
+  });
+});
+
+test('Missing store.', function(t) {
+  var db = new DB('test', 1, {test: {indices: {x: {}}}});
+  return db.query('missing').then(function(res) {
+    t.same(res.results, []);
+    t.is(res.total_entries, 0);
+    db.close().then(function(){ t.end(); });
+  });
+});
+
+test('Overwrite values.', function(t) {
+  var db = new DB('test', 1, {test: {indices: {x: {}}}});
+  return db.put('test', {id: 1, x: 1}).then(function() {
+    return db.put('test', {id: 1, x: 2}).then(function() {
+      return db.query('test').then(function(res) {
+        t.same(res.results, [{id: 1, x: 2}]);
+        db.close().then(function(){ t.end(); });
+      });
+    });
+  });
+});
+
+test('Clear store.', function(t) {
+  var db = new DB('test', 1, {test: {indices: {x: {}}}});
+  return db.put('test', [{id: 1, x: 1}, {id: 2, x: 2}]).then(function() {
+    return db.clear('test').then(function() {
+      return db.query('test').then(function(res) {
+        t.same(res.results, []);
+        db.close().then(function(){ t.end(); });
+      });
+    });
+  });
+});
+
+test('Clear db.', function(t) {
+  var db = new DB('test', 1, {test: {indices: {x: {}}}});
+  return db.put('test', [{id: 1, x: 1}]).then(function() {
+    return db.clear().then(function() {
+      return db.query('test').then(function(res) {
+        t.same(res.results, []);
+        db.close().then(function(){ t.end(); });
+      });
+    });
+  });
+});
+
+test('Clear missing store.', function(t) {
+  var db = new DB('test', 1, {test: {indices: {x: {}}}});
+  return db.put('test', [{id: 1, x: 1}, {id: 2, x: 2}]).then(function() {
+    return db.clear(['test', 'missing']).then(function() {
+      return db.query('test').then(function(res) {
+        t.same(res.results, []);
+        db.close().then(function(){ t.end(); });
+      });
+    });
+  });
+});
+
+test('Total Entries.', function(t) {
+  var db = new DB('test', 1, {test: {indices: {x: {}}}});
+  var models = [
+    {id: 1},
+    {id: 2},
+    {id: 3},
+    {id: 4}
+  ];
+  return db.put('test', models).then(function() {
+    return db.query('test').then(function(res) {
+      t.is(res.total_entries, 4);
+      db.close().then(function(){ t.end(); });
+    });
+  });
+});
+
+test('Total Entries, Paged.', function(t) {
+  var db = new DB('test', 1, {test: {indices: {x: {}}}});
+  var models = [
+    {id: 1},
+    {id: 2},
+    {id: 3},
+    {id: 4}
+  ];
+  return db.put('test', models).then(function() {
+    return db.query('test', {page: 1, per_page: 3}).then(function(res) {
+      t.same(res.results, models.slice(0, 3));
+      t.is(res.total_entries, 4);
+      db.close().then(function(){ t.end(); });
+    });
+  });
+});
+
+test('Total Entries, Paged.', function(t) {
+  var db = new DB('test', 1, {test: {indices: {x: {}}}});
+  var models = [
+    {id: 1},
+    {id: 2},
+    {id: 3},
+    {id: 4}
+  ];
+  return db.put('test', models).then(function() {
+    return db.query('test', {page: 2, per_page: 3}).then(function(res) {
+      t.same(res.results, models.slice(3));
+      t.is(res.total_entries, 4);
+      db.close().then(function(){ t.end(); });
+    });
+  });
+});
+
+test('Per page default is 10', function(t) {
+  var db = new DB('test', 1, {test: {indices: {x: {}}}});
+  models = [
+    {id: 1},
+    {id: 2},
+    {id: 3},
+    {id: 4},
+    {id: 5},
+    {id: 6},
+    {id: 7},
+    {id: 8},
+    {id: 9},
+    {id: 10},
+    {id: 11},
+    {id: 12}
+  ];
+  return db.put('test', models).then(function() {
+    return db.query('test', {page: 1}).then(function(res) {
+      t.same(res.results, models.slice(0, 10));
+      t.is(res.total_entries, 12);
+      db.close().then(function(){ t.end(); });
+    });
+  });
+});
+
+test('get', function(t) {
+  var db = new DB('test', 1, {test: {indices: {x: {}}}});
+  return db.put('test', {id: 1, x: 1}).then(function() {
+    return db.get('test', 1).then(function(data) {
+      t.same(data, {id: 1, x: 1});
+      db.close().then(function(){ t.end(); });
+    });
+  });
+});
+
+test('get missing', function(t) {
+  var db = new DB('test', 1, {test: {indices: {x: {}}}});
+  return db.get('test', 1).then(function(data) {
+    t.is(data, undefined);
+    db.close().then(function(){ t.end(); });
+  });
+});
+
+test('keyPath', function(t) {
+  var db = new DB('test', 1, {test: {keyPath: 'x', indices: {x: {}}}});
+  return db.put('test', {x: 3}).then(function() {
+    return db.get('test', 3).then(function(data) {
+      t.same(data, {x: 3});
+      db.close().then(function(){ t.end(); });
+    });
+  });
+});
